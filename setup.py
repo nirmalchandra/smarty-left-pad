b894e1ebd61ca9159e6032aa776f885059f34af1
Nirmal Nayak <nchandra@jappsconsultants.com>
2021-04-19 09:33:09 +0100

    version bumped

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='smarty-left-pad',
-    version='1.0.0',
+    version='1.0.1',
     packages=['smarty_left_pad'],
     url='https://github.com/nirmalchandra/smarty-left-pad',
     license='Apache 2.0',
